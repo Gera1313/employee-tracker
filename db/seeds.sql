dbf033d6cd3ed1215c14c3d0a1aeb556c64fc635
Mark Anthony <touya1313@hotmail.com>
2023-09-01 02:31:01 -0600

    finished seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,15 +6,15 @@ VALUES ("sales"),
        ("loss prevention");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("sales"),
-       ("customer service"),
-       ("tech support"),
-       ("legal"),
-       ("loss prevention");
+VALUES ("sales manager", 45000, 1 ),
+       ("customer service rep", 40000, 3),
+       ("tech support agent", 50000, 3),
+       ("legal analyst", 90000, 5),
+       ("investigator", 70000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("sales"),
-       ("customer service"),
-       ("tech support"),
-       ("legal"),
-       ("loss prevention");
+VALUES ("Lance", "Lopez", 1, NULL ),
+       ("Justin", "Kaposky", 2, 3),
+       ("Tom", "Delonge", 3, NULL),
+       ("Lilly", "Palmer", 4, 5),
+       ("Charlotte", "DeWitte", 5, NULL);
