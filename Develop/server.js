3bc0189a941bd66d921f18239e4477255239ed5c
Mark Anthony <touya1313@hotmail.com>
2023-09-01 17:01:03 -0600

    started inquirer prompts in server file

--- Develop/server.js
+++ Develop/server.js
@@ -13,3 +13,21 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employees_db database.`)
 );
+
+startInquire();
+
+function startInquire() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: ["View all Departments", "View all Roles", "View all Employees",
+            "Add a department", "Add a role", "Add an employee",
+            "Update employee role",
+            "Exit"],
+        },
+    ])
+}
+
+// NOTE: looked at INS and STU code from the Modules to get boilerplate code and examples. 
