4f9829477cccff56adfd286641f5ad07015ea07a
Mark Anthony <touya1313@hotmail.com>
2024-02-15 23:53:46 -0700

    corrected some typos in the schema and seeds.sql files

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
@@ -21,7 +21,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NULL
+    manager_id INT NULL,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,20 +1,20 @@
 INSERT INTO department (name)
-VALUES ("sales"),
-       ("customer service"),
-       ("accounting"),
-       ("legal"),
-       ("loss prevention");
+VALUES ('sales'),
+       ('customer service'),
+       ('accounting'),
+       ('legal'),
+       ('loss prevention');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("sales manager", 45000, 1 ),
-       ("customer service rep", 40000, 3),
-       ("tech support agent", 50000, 3),
-       ("legal analyst", 90000, 5),
-       ("investigator", 70000, 5);
+VALUES ('sales manager', 45000, 1 ),
+       ('customer service rep', 40000, 3),
+       ('tech support agent', 50000, 3),
+       ('legal analyst', 90000, 5),
+       ('investigator', 70000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Lance", "Lopez", 1, NULL ),
-       ("Justin", "Kaposky", 2, 3),
-       ("Tom", "Delonge", 3, NULL),
-       ("Lilly", "Palmer", 4, 5),
-       ("Charlotte", "DeWitte", 5, NULL);
+VALUES ('Lance', 'Lopez', 1, NULL ),
+       ('Justin', 'Kaposky', 2, 3),
+       ('Tom', 'Delonge', 3, NULL),
+       ('Lilly', 'Palmer', 4, 5),
+       ('Charlotte', 'DeWitte', 5, NULL);
