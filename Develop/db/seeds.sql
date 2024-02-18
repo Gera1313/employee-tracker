2a142b0f27c035210eb4471ada9809b713c82a00
Mark Anthony <touya1313@hotmail.com>
2024-02-17 19:42:04 -0700

    seed not working. Corrected seed file to see if it works

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -9,12 +9,12 @@ INSERT INTO role (title, salary, department_id)
 VALUES ('sales manager', 45000, 1 ),
        ('customer service rep', 40000, 3),
        ('tech support agent', 50000, 3),
-       ('legal analyst', 90000, 5),
-       ('investigator', 70000, 5);
+       ('legal analyst', 90000, 4),
+       ('investigator', 70000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Lance', 'Lopez', 1, NULL ),
        ('Justin', 'Kaposky', 2, 3),
        ('Tom', 'Delonge', 3, NULL),
-       ('Lilly', 'Palmer', 4, 5),
+       ('Lilly', 'Palmer', 4, NULL),
        ('Charlotte', 'DeWitte', 5, NULL);
